280a3eed7fa98fee7a4953b8fdd602580dca9dce
Leon <tonge3199@gmail.com>
2025-03-26 09:47:32 +0800

    A: Added Initial files

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot))) // Handle the root path using the mux
+
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
